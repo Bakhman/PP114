590635b68ad13cd5bf69c7dba0fb1e93bb0f11b4
Bakhman <begaev7@gmail.com>
2022-06-10 17:42:47 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -12,52 +12,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-        /*Util util = new Util();
-        Session session = null;*/
-
-        // ===SAVE==
-        /*session = util.getSession();
-        session.beginTransaction();
-        User user = new User("Arnold", "Shwarcenneger", (byte) 66);
-        System.out.println(user);
-        session.save(user);
-        System.out.println(user);
-        session.getTransaction().commit();*/
-
-        // === create LIST USERS ==
-        /*session = util.getSession();
-        session.beginTransaction();
-        List<User> userList = session.createQuery("select i from User i", User.class)
-                .getResultList();
-        System.out.println(userList);
-        session.getTransaction().commit();*/
-
-        // === DELETE ===
-        /*session = util.getSession();
-        session.beginTransaction();
-        session.delete(userList.get(0));
-        session.getTransaction().commit();*/
-
-        // === SQLQuery===
-       /* session = util.getSession();
-        session.beginTransaction();
-        session.createSQLQuery("create table users2 (id int not null auto_increment primary key);");
-        session.getTransaction().commit();*/
-
-
-
-
-        //session.close();
         UserService userService = new UserServiceImpl();
-        /*userService.createUsersTable();
+        userService.createUsersTable();
         userService.saveUser("ADAM", "SMITH", (byte) 17);
         userService.saveUser("JONES", "DOE", (byte) 44);
         userService.saveUser("MARTIN", "VAN HARTEN", (byte) 64);
         userService.saveUser("CLARK", "KENT", (byte) 33);*/
         //userService.removeUserById(5l);
-        //userService.getAllUsers();
+        userService.getAllUsers();
         userService.cleanUsersTable();
-        //userService.dropUsersTable();
+        userService.dropUsersTable();
+        
+        session.close();
     }
 }
